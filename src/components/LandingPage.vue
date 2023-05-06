f332a891de98e28c2532494fc4c14c042fe26db0
teboho <tsekopa@csir.co.za>
2023-05-06 12:27:22 +0200

    added components

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,39 +1,108 @@
 <template>
-  <v-card>
+  <v-card >
     <v-layout>
-      <v-app-bar color="surface-variant" title="Planet Venus"><img src="../assets/planet_venus.jpg" alt=""></v-app-bar>
-        <v-carousel hide-delimiters>
-          <v-carousel-item
-            v-for="(item,i) in items"
-            :key="i"
-            :src="item.src"
-          ></v-carousel-item>
-        </v-carousel>
-      <v-main style="min-height: 300px;"></v-main>
+      <v-app-bar>
+        <v-col>
+        </v-col>
+        <v-col>
+        </v-col>
+        <v-col><h3>Porn Videos</h3></v-col>
+        <v-col></v-col>
+        <v-col>
+          <v-text-field label="Search" append-icon="mdi-magnify" filled outlined></v-text-field>
+        </v-col>
+      </v-app-bar>
+      <v-navigation-drawer
+        expand-on-hover
+        rail
+      >
+        <v-list>
+          <v-list-item
+            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+            title="Sandra Adams"
+            subtitle="sandra_a88@gmailcom"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-folder" title="My Items" value="myitems"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
+          <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+        </v-list>
+        <template v-slot:append>
+          <div class="pa-2">
+            <v-btn block color="error" >
+              <v-icon>mdi-logout</v-icon>
+            </v-btn>
+          </div>
+        </template>
+      </v-navigation-drawer>
+      <v-main style="min-height: 300px;">
+        <comp-tabs></comp-tabs>
+      </v-main>
     </v-layout>
   </v-card>
 </template>
 
 <script>
+import CompTabs from './CompTabs.vue';
 export default {
   name: 'LandingPage',
+  components: {CompTabs},
   data () {
     return {
-      items: [
-        {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
-        },
-        {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
-        },
-        {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
-        },
-        {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
-        }
-      ]
+      drawer: false,
+      group: null,
+      tab: null,
+        items: [
+          'web', 'shopping', 'videos', 'images', 'news',
+        ],
+      videoUrl: "https://www.xvideos.com/video76083555/detention_w_ms._del_rio",
+      youtubeURL: "",
+      result: "",
+      xresults: ""
     }
+  },
+  watch: {
+    group() {
+      this.drawer = false
+    }
+  },
+  methods: {
+    loadURL() {
+      const youtubeEmbedTemplate = "https://www.xvideos.com/embedframe/";
+      const url = this.youtubeURL.split(
+        /(vi\/|v%3D|v=|\/v\/|youtu\.be\/|\/embed\/)/
+      );
+      console.log("url", url);
+      const YId =
+        undefined !== url[2] ? url[2].split(/[^0-9a-z_/\\-]/i)[0] : url[0];
+      console.log("YId", YId);
+      if (YId === url[0]) {
+        console.log("not a youtube link");
+      } else {
+        console.log("it's  a youtube video");
+      }
+      const topOfQueue = youtubeEmbedTemplate.concat(YId);
+      console.log("topOfQueue", topOfQueue);
+      this.result = topOfQueue;
+      this.youtubeURL = "";
+    },
+    loadThumbnail(){
+      const videoEmbedTemplate = "https://www.xvideos.com/embedframe/";
+      const vidUrl = this.videoUrl.split(/video|\//)
+      const vidId = undefined !== vidUrl[1] ? vidUrl[1].split(/[^0-9]/i)[0] : vidUrl[0]
+      if (vidId === vidUrl[0]) {
+        console.log("not video link")
+      } else {
+        console.log("this is the link")
+      }
+      const frame = videoEmbedTemplate.concat(vidId)
+      this.xresults = frame
+    }
+    
   }
 }
 </script>
--- src/components/WatchPage.vue
+++ src/components/WatchPage.vue
@@ -34,15 +34,74 @@
           height="48"
           location="bottom"
         ></v-app-bar>
-  
         <v-main>
-          <v-container
-            class="d-flex justify-center align-center text-h5"
-            style="min-height: 300px;"
-          >
-            Content
-          </v-container>
+          <v-card>
+    <v-tabs
+      v-model="tab"
+      color="deep-purple-accent-4"
+      align-tabs="center"
+    >
+      <v-tab :value="1">Landscape</v-tab>
+      <v-tab :value="2">City</v-tab>
+      <v-tab :value="3">Abstract</v-tab>
+    </v-tabs>
+    <v-window v-model="tab">
+      <v-window-item
+        v-for="n in 3"
+        :key="n"
+        :value="n"
+      >
+        <v-container fluid>
+          <v-row>
+            <v-col
+              v-for="i in 6"
+              :key="i"
+              cols="12"
+              md="4"
+            >
+              <v-img
+                :src="`https://picsum.photos/500/300?image=${i * n * 5 + 10}`"
+                :lazy-src="`https://picsum.photos/10/6?image=${i * n * 5 + 10}`"
+                aspect-ratio="1"
+              ></v-img>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-window-item>
+    </v-window>
+  </v-card>
+            <iframe :src="xresults" frameborder=0 width=510 height=400 scrolling=no allowfullscreen=allowfullscreen></iframe>
         </v-main>
       </v-layout>
     </v-card>
   </template>
+  <script>
+    export default {
+      
+      name: 'LandingPage',
+      data () {
+        return {
+          tab: null,
+          videoUrl: "https://www.xvideos.com/video76083555/detention_w_ms._del_rio",
+          youtubeURL: "",
+          result: "",
+          xresults: ""
+        }
+      },
+      methods: {
+        loadThumbnail(){
+          const videoEmbedTemplate = "https://www.xvideos.com/embedframe/";
+          const vidUrl = this.videoUrl.split(/video|\//)
+          const vidId = undefined !== vidUrl[1] ? vidUrl[1].split(/[^0-9]/i)[0] : vidUrl[0]
+          if (vidId === vidUrl[0]) {
+            console.log("not video link")
+          } else {
+            console.log("this is the link")
+          }
+          const frame = videoEmbedTemplate.concat(vidId)
+          this.xresults = frame
+        }
+        
+      }
+    }
+</script>
